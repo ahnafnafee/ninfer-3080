Wallawalla47/ninfer-custom

feat(serve): add --thinking-budget-message launch flag
Lets operators replace the built-in end-of-thinking control message with a custom one when a request hits its thinking budget. The frontend appends the canonical close serialization to a message that lacks it, and the existing startup validation (tokenizer round-trip, presentation, no default terminal tokens) now applies to the effective suffix. Empty keeps the model's built-in behaviour.

src/serve/generation_service.cpp
@@ -235,6 +235,7 @@ GenerationService::GenerationService(ServeOptions options, StartupObserver start
     ninfer::EngineOptions engine_options;
     engine_options.artifact_path            = options_.artifact_path;
     engine_options.chat_template_path       = options_.chat_template_path;
+    engine_options.thinking_budget_message  = options_.thinking_budget_message;
     engine_options.device                   = options_.device;
     engine_options.max_context              = options_.max_context;
     engine_options.rope_yarn_factor          = options_.rope_yarn_factor;

ahnafnafee/ninfer-3080

src/serve/generation_service.cpp
@@ -271,6 +271,7 @@ ninfer::EngineOptions make_engine_options(const ServeOptions& options) {
     engine_options.gdn_state_fp16           = options.gdn_state_fp16;
     engine_options.rope_yarn                = options.rope_yarn;
     engine_options.structured_output        = options.structured_output;
+    engine_options.thinking_budget_message  = options.thinking_budget_message;
     engine_options.cuda_graph_allowance_bytes =
         static_cast<std::size_t>(options.cuda_graph_allowance_mib) << 20;
     engine_options.wddm_evictable_budget    = options.wddm_evictable_budget;